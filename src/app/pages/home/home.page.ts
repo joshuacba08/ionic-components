2f95bdf0ee0473b33e45a47fc37a459f04c2e480
Josué Anderson <47667368+joshuacba08@users.noreply.github.com>
2021-08-19 17:09:13 -0300

    ion-card

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -33,6 +33,11 @@ export class HomePage implements OnInit {
       icon: 'radio-button-off-outline',
       name: 'Buttons',
       redirectTo: '/button'
+    },
+    {
+      icon: 'card-outline',
+      name: 'Cards',
+      redirectTo: '/card'
     }
   ];
   constructor() { }
