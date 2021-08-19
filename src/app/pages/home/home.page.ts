139f7b023489ff66ae301121acaff9e942556b0b
Josué Anderson <47667368+joshuacba08@users.noreply.github.com>
2021-08-19 14:31:25 -0300

    ion-avatar & ion-chip

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -23,6 +23,11 @@ export class HomePage implements OnInit {
       icon: 'alert-circle-outline',
       name: 'Alert',
       redirectTo: '/alert'
+    },
+    {
+      icon: 'beaker-outline',
+      name: 'Avatar',
+      redirectTo: '/avatar'
     }
   ];
   constructor() { }
